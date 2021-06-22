750aae2e0f5809714f3d3292f3ea71f12182c45a
yavuzs <yavuzselim@sabanciuniv.edu>
2021-06-22 12:57:18 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
